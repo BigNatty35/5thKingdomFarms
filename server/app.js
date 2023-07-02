5ae38555db1390c061e5617ed9f369787d2c31ee
Carey Johnson <carey@mindrglobal.com>
2023-07-02 18:28:50 -0400

    merged and changed port

--- server/app.js
+++ server/app.js
@@ -11,22 +11,11 @@ const pool = new Pool({
   port: 5434, // Default PostgreSQL port
 });
 
-
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'fifthkingdom',
-  password: '5thKing',
-  port: 5434, // Default PostgreSQL port
-});
-
 app.get("/", (req, res) => {
   res.send("Hey baby is working!")
 });
 
-const PORT = 8080;
+const PORT = 5050;
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT:${PORT}`);
