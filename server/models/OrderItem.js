5287347b09ce4e49cab2082a3dbbcd83782c0733
Carey Johnson <carey@mindrglobal.com>
2023-07-04 13:49:02 -0400

    added OrderItem table/model

---
+++ server/models/OrderItem.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const OrderItem = sequelize.define(
+    'OrderItem',
+    {
+      order_item_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      order_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Orders',
+          key: 'order_id',
+        },
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Products',
+          key: 'product_id',
+        },
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      item_price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
+    },
+    {}
+  );
+
+  return OrderItem;
+};
